40e623843592fe0e4f0f7115aba5f8f4b571a431
bkha0 <bashir50alabir@gmail.com>
2025-10-24 11:40:42 +0300

    commit for 1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector('.menu');
+const menuBtn = document.getElementById('menuBtn');
+const menuIcon = menuBtn.querySelector('i');
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('show');
+    if (menu.classList.contains('show')) {
+        menuIcon.classList.replace('bx-menu-alt-right', 'bx-x');
+    } else {
+        menuIcon.classList.replace('bx-x', 'bx-menu-alt-right');
+    }
+});
+const menuLinks = document.querySelectorAll('.menu a');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('show');
+        menuIcon.classList.replace('bx-x', 'bx-menu-alt-right');
+    });
+});
+document.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
+        menu.classList.remove('show');
+        menuIcon.classList.replace('bx-x', 'bx-menu-alt-right');
+    }
+});
